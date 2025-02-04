6c16a9a8f77cf80f484c99005aa0a83f630780cb
x-desu <amitesh5q2@gmail.com>
2025-02-04 22:00:15 +0530

    section

--- app/components/Section.jsx
+++ app/components/Section.jsx
@@ -25,41 +25,43 @@ const Section = () => {
       <h3 className="font-bold text-black tracking-widest">HELLO THERE</h3>
       <h1 className="font-semibold text-5xl  text-white py-4">We Are Dyvos</h1>
       <hr className="w-96 h-[1px]  my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-700" />
-      <p className="max-h-64 my-4 line-clamp-6 tracking-wide leading-relaxed text-xl text-center text-black font-lora max-w-6xl">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt.
+      <p className="max-h-64  my-8 lg:text-3xl lg:max-w-7xl line-clamp-6 tracking-wide leading-relaxed text-xl text-center text-black font-lora max-w-6xl">
+        At DYVOS, we provide AI-driven solutions that enhance efficiency, secure
+        data, and streamline operations. Our intelligent CMS platforms,
+        reporting tools, and collaboration systems empower healthcare
+        professionals to optimize workflows and improve outcomes. As a new
+        company, we bring fresh ideas and cutting-edge technology to
+        revolutionize healthcare, ensuring smarter, more effective solutions for
+        the future.
       </p>
 
-      <div className="flex gap-4">
+      <div className="flex flex-col gap-4">
         <div className="flex flex-col gap-8 justify-center items-center ">
+          {/*
           <div className="flex flex-col gap-4  items-center justify-center tracking-wider ">
             <h1 className="text-5xl sm:text-7xl text-center font-bold text-white">
               <AnimateNum value={127} />
             </h1>
             <p className="text-black  font-semibold">Awards Received</p>
-          </div>
+          </div> */}
           {/* <hr className="w-[1px] h-36 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-700" /> */}
           <div className="flex flex-col gap-4 items-center justify-center  tracking-wider ">
             <h1 className="text-5xl sm:text-7xl text-center font-bold text-white w-40">
-              <AnimateNum value={1505} />
-            </h1>
-            <p className="text-black font-semibold">Cups of Coffee</p>
-          </div>
-        </div>
-
-        <hr className="w-[1px] h-64 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-700" />
-        <div className="flex flex-col gap-8 justify-center items-center ">
-          <div className="flex flex-col gap-4 items-center justify-center  tracking-wider ">
-            <h1 className="text-5xl sm:text-7xl text-center font-bold text-white">
               <AnimateNum value={109} />
             </h1>
             <p className="text-black font-semibold">Projects Done</p>
           </div>
-          {/* <hr className="w-[1px] h-36 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-700" /> */}
+        </div>
+
+        {/* <hr className="w-64 h-[1px] mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-700" /> */}
+        <div className="flex  gap-8 justify-center items-center ">
+          <div className="flex flex-col gap-4 items-center justify-center  tracking-wider ">
+            <h1 className="text-5xl sm:text-7xl text-center font-bold text-white">
+              <AnimateNum value={1505} />
+            </h1>
+            <p className="text-black font-semibold">Cups of Coffee</p>
+          </div>
+          <hr className="w-[1px] h-36 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-700" />
           <div className="flex flex-col gap-4 items-center justify-center tracking-wider ">
             <h1 className="text-5xl sm:text-7xl text-center font-bold text-white  w-40">
               {" "}
